adea3300d1428d0c8db44af5602ed1df8214d22b
Harshit <uk09harshit@gmail.com>
2025-12-11 19:50:59 +0530

    Add files via upload

---
+++ BLACKJACK/script.js
@@ -0,0 +1,28 @@
+let soc = document.getElementById("y")
+let count = 0;
+function increment() {
+    count += 1;
+    soc.innerText = count;
+}
+function increment2() {
+    count += 2;
+    soc.innerText = count;
+}
+function increment3() {
+    count += 3;
+    soc.innerText = count;
+}
+let soc2 = document.getElementById("z")
+let count2 = 0;
+function increment4() {
+    count2 += 1;
+    soc2.innerText = count2;
+}
+function increment5() {
+    count2 += 2;
+    soc2.innerText = count2;
+}
+function increment6() {
+    count2 += 3;
+    soc2.innerText = count2;
+}
