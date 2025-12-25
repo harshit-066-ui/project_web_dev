740f9362bc7be93445b00a476e32ca95ba5bf41b
Harshit <uk09harshit@gmail.com>
2025-12-25 13:24:53 +0530

    Add files via upload

---
+++ first react static page/src/app.jsx
@@ -0,0 +1,13 @@
+
+import Main from "./component/main"
+import NavBar from "./component/navbar"
+export default function App(params) {
+    return (
+        <>
+
+            <NavBar />
+            <Main />
+        </>
+    )
+
+}
