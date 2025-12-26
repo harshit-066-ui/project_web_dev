23a84e67cf0e54e166423af72204b291a5ddad5c
Harshit <uk09harshit@gmail.com>
2025-12-26 09:31:16 +0530

    Create Entry component for travel journal

---
+++ travel journal/components/Entry.jsx
@@ -0,0 +1,28 @@
+export default function Entry(props) {
+    console.log(props)
+    return (
+        <article className="journal-entry">
+            <div className="main-image-container">
+                <img 
+                    className="main-image" 
+                    src={props.entry.img.src}
+                    alt={props.entry.img.alt}
+                />
+            </div>
+
+            {}
+            <div className="info-container">
+                <img className="marker" src="../images/marker.png" alt="map marker icon" />
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entry.googleMapsLink}>
+                    View on Google Maps
+                </a>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
+                <p className="entry-text">
+                   {props.entry.text}
+                </p>
+            </div>
+        </article>
+    )
+}
