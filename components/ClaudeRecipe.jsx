85087dd83b363b0951b48c383cb28d2ef1cf12db
Harshit <219007351+harshit-066-ui@users.noreply.github.com>
2025-12-27 10:20:41 +0530

    Delete components directory

--- components/ClaudeRecipe.jsx
+++
@@ -1,9 +0,0 @@
-import ReactMarkdown from "react-markdown"
-export default function ClaudeRecipe(props) {
-    return (
-        <section className="suggested-recipe-container" aria-live="polite">
-            <h2>Chef MC recommends:</h2>
-            <ReactMarkdown>{props.recipe}</ReactMarkdown>
-        </section>
-    )
-}
--- components/header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <img src="chef-claude-icon.png"/>
-            <h1>Chef MC</h1>
-        </header>
-    )
-}
