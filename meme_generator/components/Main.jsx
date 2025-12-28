9263a7c82b99003885730ba4a3b8349d88a2c903
Harshit <219007351+harshit-066-ui@users.noreply.github.com>
2025-12-28 18:07:23 +0530

    Add files via upload

---
+++ meme_generator/components/Main.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react"
+
+export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getMemeImage() {
+        if (allMemes.length === 0) return
+
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: newMemeUrl
+        }))
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <label>
+                    Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>
+                    Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <button onClick={getMemeImage}>
+                    Get a new meme image 🖼
+                </button>
+            </div>
+
+            <div className="meme">
+                <img src={meme.imageUrl} alt="Meme" />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
